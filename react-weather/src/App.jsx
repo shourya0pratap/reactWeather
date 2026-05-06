9ba6b392754dc8f7ffe5639a6c60735d3c43f72c
sp_3141 <07shourya.pratap@gmail.com>
2026-05-06 20:20:10 +0530

    Updated CityInput

--- react-weather/src/App.jsx
+++ react-weather/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./components/Hero";
+import CityInput from "./components/CityInput";
 
 const App = () => {
   return (
     <>
       <Hero />
+      <CityInput/>
     </>
   );
 };
--- react-weather/src/components/CityInput.jsx
+++ react-weather/src/components/CityInput.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const CityInput = () => {
-  return (
-    <div>CityInput</div>
-  )
-}
+  const [city, setCity] = useState("");
+  const handleSubmit = () => {};
 
-export default CityInput
+  return (
+    <form className="bg-white/90 backdrop-blur-xl w-1/3 p-8 rounded-[14px] flex flex-col gap-5 border border-white/10">
+      <label for="cityName">Location Search</label>
+      <input
+        id="cityName"
+        type="text"
+        name="name"
+        value={city}
+        placeholder="Search city"
+        onChange={(e) => setCity(e.target.value)}
+        required
+      />
+      <button type="submit" onClick={handleSubmit}>
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default CityInput;
